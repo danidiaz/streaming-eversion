2de6e2391ea274d53d838e80321ec9316b8dd1b4
DDC <diaz_carrete@yahoo.com>
2016-06-05 12:57:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## What's in this library?
+
+## When to use this library?
+
+## Where can I find working examples for this library?
+
---
+++ src/Control/Foldl/Streaming.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE CPP #-}
+
+module Control.Foldl.Streaming (
+) where
+
