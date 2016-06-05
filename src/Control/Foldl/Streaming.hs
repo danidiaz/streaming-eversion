70952923c3fcffb68a153e6588282df97b9cc695
DDC <diaz_carrete@yahoo.com>
2016-06-05 13:39:06 +0200

    Some signatures (that don't compile)

--- src/Control/Foldl/Streaming.hs
+++ src/Control/Foldl/Streaming.hs
@@ -1,11 +1,31 @@
-{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Control.Foldl.Streaming (
 ) where
 
+import           Data.Functor.Identity
+
+import           Control.Foldl (FoldM)
+import qualified Control.Foldl as Foldl
+import           Streaming (Stream,Of)
+import qualified Streaming as Streaming
+
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.Free
+
+-- -- | Workaround for the need of -XImpredicativeTypes.
+-- newtype StreamOf a m r = StreamOf { getStream :: Stream (Of a) m r }  
+
+-- evert :: forall x a . (forall m r. Monad m => StreamOf a m r -> m (x,r)) -> FoldM Identity a x
+-- evert = undefined
+
+evert :: (forall m r. Monad m => Stream (Of a) m r -> m (x,r)) -> FoldM Identity a x
+evert = undefined
+
+evertIO :: MonadIO m 
+        => (forall t r. (MonadTrans t, MonadIO (t m)) => Stream (Of a) (t m) r -> t m (x,r)) 
+        -> FoldM m a r
+evertIO = undefined
